447bf35eeee8c59901a632dc03ca17dd40995262
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-05-21 11:19:56 +0200

    js code created

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
-'use strict';
+'use strict'
+const right = 'ArrowRight'
+const left = 'ArrowLeft'
+const up = 'ArrowUp'
+const down = 'ArrowDown'
 
-console.log('it works!');
+const keyArrows = [right, left, up, down]
+
+const pacMan = document.querySelector('.entity--pac')
+document.addEventListener('keyup', (event) => {
+  if (keyArrows.includes(event.code)) {
+    pacMan.classList.toggle('entity--pac-closed')
+    pacMan.classList.remove('entity--pac-up', 'entity--pac-down', 'entity--pac-left', 'entity--pac-right')
+    if (event.code === right) {
+      pacMan.classList.add('entity--pac-right')
+    }
+
+    if (event.code === left) {
+      pacMan.classList.add('entity--pac-left')
+    }
+
+    if (event.code === up) {
+      pacMan.classList.add('entity--pac-up')
+    }
+
+    if (event.code === down) {
+      pacMan.classList.add('entity--pac-down')
+    }
+  }
+})
